9930b5beb0c7f14482b31d4cf2453f424772de90
maojiubb <maojiu-bb@outlook.com>
2023-05-29 20:19:23 +0800

    init

---
+++ src/components/test/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <view>
+    <text>test</text>
+    <view>veiw</view>
+    <image src="@/static/logo.png" mode="scaleToFill" />
+  </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
